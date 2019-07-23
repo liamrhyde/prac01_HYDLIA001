1b3b9f789f20c852b36e0edcc0c6d92cf87ab48b
liamrhyde <hydlia001@myuct.ac.za>
2019-07-23 17:09:15 +0100

    Python file to turn on single LED using pin 11 as out and pin 9 as ground

--- prac01_HYDLIA001.py
+++ prac01_HYDLIA001.py
@@ -3,10 +3,10 @@
 Python Practical Template
 Keegan Crankshaw
 Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: <Liam Hyde>
+Student Number: <HYDLIA001>
+Prac: <Prac 01>
+Date: <22/07/2019>
 """
 
 # import Relevant Librares
@@ -14,8 +14,9 @@ import RPi.GPIO as GPIO
 
 # Logic that you write
 def main():
-    print("write your logic here")
-
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.output(11, 1)
 
 # Only run the functions if 
 if __name__ == "__main__":
