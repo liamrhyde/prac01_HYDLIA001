bfcb228a682d5676bf890cf0ecb383f4291942a6
liamrhyde <hydlia001@myuct.ac.za>
2019-07-23 18:31:02 +0100

    Second program. Toggles LED on and off everytwo seconds

--- prac01_HYDLIA001.py
+++ prac01_HYDLIA001.py
@@ -1,34 +1,1 @@
-#!/usr/bin/python3
-"""
-Python Practical Template
-Keegan Crankshaw
-Readjust this Docstring as follows:
-Names: <Liam Hyde>
-Student Number: <HYDLIA001>
-Prac: <Prac 01>
-Date: <22/07/2019>
-"""
-
-# import Relevant Librares
-import RPi.GPIO as GPIO
-
-# Logic that you write
-def main():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(11, GPIO.OUT)
-    GPIO.output(11, 1)
-
-# Only run the functions if 
-if __name__ == "__main__":
-    # Make sure the GPIO is stopped correctly
-    try:
-        while True:
-            main()
-    except KeyboardInterrupt:
-        print("Exiting gracefully")
-        # Turn off your GPIOs here
-        GPIO.cleanup()
-    except e:
-        GPIO.cleanup()
-        print("Some other error occurred")
-        print(e.message)
+#!/usr/bin/python3"""Python Practical TemplateKeegan CrankshawReadjust this Docstring as follows:Names: <names>Student Number: <studnum>Prac: <Prac Num>Date: <dd/mm/yyyy>"""# import Relevant Libraresimport RPi.GPIO as GPIOimport time# Logic that you writedef main():    #print("write your logic here")    GPIO.setmode(GPIO.BOARD)    GPIO.setup(11, GPIO.OUT)    GPIO.output(11, 1)    time.sleep(2)    GPIO.output(11, 0)    time.sleep(2)# Only run the functions ifif __name__ == "__main__":    # Make sure the GPIO is stopped correctly    try:        while True:            main()    except KeyboardInterrupt:        print("Exiting gracefully")        # Turn off your GPIOs here        GPIO.cleanup()   # except e:   #     GPIO.cleanup()   #     print("Some other error occurred")   #     print(e.message)
